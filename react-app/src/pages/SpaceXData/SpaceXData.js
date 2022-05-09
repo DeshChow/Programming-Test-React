9070ac05dff9e521f4fd1fd37496936b9805a755
Desh Chowdhury <deshchow001@gmail.com>
2022-05-09 21:08:32 +0600

    Pagination added to make performance better

--- react-app/src/pages/SpaceXData/SpaceXData.js
+++ react-app/src/pages/SpaceXData/SpaceXData.js
@@ -1,25 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import Card from "../../Components/Card/Card";
 import axiosInstance from "../../services/axiosInstance";
 
+const PER_PAGE = 9;
+
 const SpaceXData = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const [lunches, setLunches] = useState([]);
 
-  axiosInstance
-    .get("/v3/launches")
-    .then((res) => {
-      setLunches(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  useEffect(() => {
+    axiosInstance
+      .get("/v3/launches")
+      .then((res) => {
+        setLunches(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+  };
+
+  const offset = currentPage * PER_PAGE;
+
+  const currentPageLunches = lunches.slice(offset, offset + PER_PAGE);
+
+  const pageCount = Math.ceil(lunches.length / PER_PAGE);
 
   return (
     <div className="container">
       <div className="row g-3">
-        {lunches &&
-          lunches.length > 0 &&
-          lunches.map((lunch) => {
+        {currentPageLunches &&
+          currentPageLunches.length > 0 &&
+          currentPageLunches.map((lunch) => {
             return (
               <div className="col-12 col-md-6 col-lg-4">
                 <Card lunch={lunch} />
@@ -27,6 +43,22 @@ const SpaceXData = () => {
             );
           })}
       </div>
+      <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
 };
