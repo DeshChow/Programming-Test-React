4db0756606965db1a88325cd9ffaf1eef54a1500
Desh Chowdhury <deshchow001@gmail.com>
2022-05-11 12:18:40 +0600

    Bug related to react-dom is fixed & project is ready to deploy

--- react-app/src/pages/SpaceXData/SpaceXData.js
+++ react-app/src/pages/SpaceXData/SpaceXData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Helmet from "react-helmet";
 import {
   getLaunches,
   getLastWeekLaunches,
@@ -78,89 +79,94 @@ const SpaceXData = () => {
   };
 
   return (
-    <div className="container">
-      <div className="w-100 d-flex justify-content-center">
-        <h1 className="display-1">SpaceX</h1>
+    <>
+      <Helmet>
+        <title>SpaceX Launches List</title>
+      </Helmet>
+      <div className="container">
+        <div className="w-100 d-flex justify-content-center">
+          <h1 className="display-1">SpaceX</h1>
+        </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <div className="w-100 mt-4 d-flex flex-column flex-md-row justify-content-between align-items-center">
+              <div className="w-75  input-group">
+                <select
+                  className="form-select shadow-sm p-3 bg-white rounded"
+                  name="launches"
+                  id="launches"
+                  defaultValue=""
+                  onChange={(e) => handleFilteringlaunches(e)}
+                  style={{ marginBottom: "2rem" }}
+                >
+                  <option value="" disabled>
+                    Select to Filter the Launches
+                  </option>
+                  <option value="last-week">Last Week Launches</option>
+                  <option value="last-month">Last Month Launches</option>
+                  <option value="last-year">Last Year Launches</option>
+                  <option value="failure">Failure Launches</option>
+                  <option value="success">Successful Launches</option>
+                  <option value="upcoming">Upcoming Launches</option>
+                </select>
+              </div>
+              <div className="w-75 input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control shadow-sm p-3 mb-3 bg-white rounded"
+                  placeholder="Search by Rocket Name"
+                  aria-label="Search by Rocket Name"
+                  aria-describedby="basic-addon2"
+                  onChange={(e) => handleSearchByName(e)}
+                />
+                <span className="input-group-text  p-3 mb-3" id="basic-addon2">
+                  SEARCH
+                </span>
+              </div>
+            </div>
+            <div className="row g-3">
+              {currentPagelaunches &&
+                currentPagelaunches.length > 0 &&
+                currentPagelaunches
+                  // eslint-disable-next-line array-callback-return
+                  .filter((launch) => {
+                    if (searchByName === "") return launch;
+                    else if (launch?.rocket?.rocket_name?.toLowerCase().includes(searchByName.toLowerCase())) return launch;
+                  })
+                  .map((launch, index) => {
+                    return (
+                      <div className="col-12 col-md-6 col-lg-4" key={index}>
+                        <Card launch={launch} />
+                      </div>
+                    );
+                  })}
+            </div>
+            {currentPagelaunches && currentPagelaunches.length > 0 && (
+              <div className="mt-4 mb-4">
+                <ReactPaginate
+                  previousLabel={"← Previous"}
+                  nextLabel={"Next →"}
+                  pageCount={pageCount}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination justify-content-center"}
+                  pageClassName={"page-item"}
+                  pageLinkClassName={"page-link"}
+                  previousClassName={"page-item"}
+                  previousLinkClassName={"page-link"}
+                  nextClassName={"page-item"}
+                  nextLinkClassName={"page-link"}
+                  breakClassName={"page-item"}
+                  breakLinkClassName={"page-link"}
+                  activeClassName={"active"}
+                />
+              </div>
+            )}
+          </>
+        )}
       </div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <div className="w-100 mt-4 d-flex flex-column flex-md-row justify-content-between align-items-center">
-            <div className="w-75  input-group">
-              <select
-                className="form-select shadow-sm p-3 bg-white rounded"
-                name="launches"
-                id="launches"
-                defaultValue=""
-                onChange={(e) => handleFilteringlaunches(e)}
-                style={{ marginBottom: "2rem" }}
-              >
-                <option value="" disabled>
-                  Select to Filter the Launches
-                </option>
-                <option value="last-week">Last Week Launches</option>
-                <option value="last-month">Last Month Launches</option>
-                <option value="last-year">Last Year Launches</option>
-                <option value="failure">Failure Launches</option>
-                <option value="success">Successful Launches</option>
-                <option value="upcoming">Upcoming Launches</option>
-              </select>
-            </div>
-            <div className="w-75 input-group mb-3">
-              <input
-                type="text"
-                className="form-control shadow-sm p-3 mb-3 bg-white rounded"
-                placeholder="Search by Rocket Name"
-                aria-label="Search by Rocket Name"
-                aria-describedby="basic-addon2"
-                onChange={(e) => handleSearchByName(e)}
-              />
-              <span className="input-group-text  p-3 mb-3" id="basic-addon2">
-                SEARCH
-              </span>
-            </div>
-          </div>
-          <div className="row g-3">
-            {currentPagelaunches &&
-              currentPagelaunches.length > 0 &&
-              currentPagelaunches
-                // eslint-disable-next-line array-callback-return
-                .filter((launch) => {
-                  if (searchByName === "") return launch;
-                  else if (launch?.rocket?.rocket_name?.toLowerCase().includes(searchByName.toLowerCase())) return launch;
-                })
-                .map((launch, index) => {
-                  return (
-                    <div className="col-12 col-md-6 col-lg-4" key={index}>
-                      <Card launch={launch} />
-                    </div>
-                  );
-                })}
-          </div>
-          {currentPagelaunches && currentPagelaunches.length > 0 && (
-            <div className="mt-4 mb-4">
-              <ReactPaginate
-                previousLabel={"← Previous"}
-                nextLabel={"Next →"}
-                pageCount={pageCount}
-                onPageChange={handlePageClick}
-                containerClassName={"pagination justify-content-center"}
-                pageClassName={"page-item"}
-                pageLinkClassName={"page-link"}
-                previousClassName={"page-item"}
-                previousLinkClassName={"page-link"}
-                nextClassName={"page-item"}
-                nextLinkClassName={"page-link"}
-                breakClassName={"page-item"}
-                breakLinkClassName={"page-link"}
-                activeClassName={"active"}
-              />
-            </div>
-          )}
-        </>
-      )}
-    </div>
+    </>
   );
 };
 
